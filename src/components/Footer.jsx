ec9d2fc643676071d98edde812138213600ef01e
Joanadgomes <joanadiogog@gmail.com>
2023-05-16 11:44:17 +0100

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='footer'>
-        <hr className='footer-hr'/>
         <h3>Designed & built by Joana Gomes</h3>
     </footer>
   )
