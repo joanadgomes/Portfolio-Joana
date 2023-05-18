924d349ce7f27ebf553e7ad24d1ba8a36231e474
Joanadgomes <joanadiogog@gmail.com>
2023-05-18 09:36:58 +0100

    github projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -24,14 +24,14 @@ function Projects() {
           </Link>
         </div>
         <div className="text-box">
-          <h2>DevHub <span> <Link to='https://github.com/joanadgomes/Project-Three-Frontend-devHub' target="_blank" className="uil uil-github-alt github-link"></Link> </span></h2>
+          <h2>DevHub</h2>
           <p className="description">
             Our platform allows users to easily share job opportunities and
             exchange tips with other developers. In addition, users can take
             quizzes to refresh their knowledge and engage in real-time
             conversations with the devHub community via our Firebase-based chat.
             As a result, we were awarded both the most difficult code-wise and
-            the best overall project prizes. <br /> <span>Ironhack March 2023.</span>
+            the best overall project prizes. <br /> <span>React.js - Javascript - HTML - CSS - MongoDB</span> <br /> <span>Ironhack March 2023.</span> <br /> <span><Link to='https://github.com/joanadgomes/Project-Three-Frontend-devHub' target="_blank" className="uil uil-github-alt github-link"></Link> </span>
           </p>
         </div>
       </article>
@@ -51,13 +51,13 @@ function Projects() {
           </Link>
         </div>
         <div className="text-box">
-          <h2>Kiddo&apos;s Care <span> <Link to='https://github.com/joanadgomes/kiddoscare' target="_blank" className="uil uil-github-alt github-link"></Link> </span></h2>
+          <h2>Kiddo&apos;s Care </h2>
           <p className="description">
             Babysitting platform that connects parents with local babysitters.
             It allows parents to search for babysitters in their area based on
             their availability, experience, and other criteria. Also allows
             individuals to create profiles as babysitters and offer their
-            services to parents. <br /> <span>React.js - Javascript - CSS - MondoDB</span> <br /> <span>Ironhack February 2023.</span> 
+            services to parents. <br /> <span>Javascript - Express.js - HTML - Handlebars - CSS - MongoDB</span> <br /> <span>Ironhack February 2023.</span> <br /> <span> <Link to='https://github.com/joanadgomes/kiddoscare' target="_blank" className="uil uil-github-alt github-link"></Link> </span>
           </p>
         </div>
       </article>
@@ -77,11 +77,11 @@ function Projects() {
           </Link>
         </div>
         <div className="text-box">
-          <h2>The Hungry Snake <span> <Link to='https://github.com/joanadgomes/Project-Three-Frontend-devHub' target="_blank" className="uil uil-github-alt github-link"></Link> </span></h2>
+          <h2>The Hungry Snake</h2>
           <p className="description">
             Classic snake game, in a marine environment with some shrimps to
-            eat, with a bonus of some trash that she need to avoid, developed
-            with Canvas and DOM manipulation. <br /> <span>Ironhack January 2023.</span>
+            eat, with a bonus of some trash that she needs to avoid, developed
+            with Canvas and DOM manipulation. <br /> <span>Javascript - HTML - CSS - Canvas</span> <br /> <span>Ironhack January 2023.</span> <br /> <span> <Link to='https://github.com/joanadgomes/Project-Three-Frontend-devHub' target="_blank" className="uil uil-github-alt github-link"></Link> </span>
           </p>
         </div>
       </article>
